819e02cef547cd3b321fb2736d067b82873f79a5
Garavirod <rodrigogarciaavila26@gmail.com>
2022-03-27 14:24:09 -0600

    verify if browwser support geolocation

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Verify if browser supports geolocation
+if( !navigator.geolocation ){
+    throw new Error("Your beowser not support GEolocation")
+}
+
+
 createApp(App).use(store).use(router).mount('#app')
