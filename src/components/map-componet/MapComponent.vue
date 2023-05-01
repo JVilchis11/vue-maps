cdb197938298ba22b47756c783c4ec443c025622
Garavirod <rodrigogarciaavila26@gmail.com>
2023-04-30 21:06:00 -0600

    Fix map view

--- src/components/map-componet/MapComponent.vue
+++ src/components/map-componet/MapComponent.vue
@@ -1,35 +1,36 @@
-<script lang="ts" src="./MapComponent.ts"/>
+<script lang="ts" src="./MapComponent.ts" />
+
 <template>
-    <div v-if="!isUserLocationReady" class="loading-map d-flex justify-content-center align-items-center">
-       <div class="text-center">
-            <h3>Wait, please</h3>
-            <span>Getting location...</span>
-        </div>        
+    <div v-if="!isUserLocationReady"
+        class="loading-map d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <h3>Espere por favor!!</h3>
+            <span>Localizando...</span>
+        </div>
     </div>
 
-    <div ref="mapElement" v-show="isUserLocationReady" class="map-container">
+    <div v-show="isUserLocationReady"
+        class="map-container"
+        ref="mapElement"
+    />
 
-    </div>
 </template>
 
-
 <style scoped>
-    .map-container{
-        position: fixed;
-        width: 100vh;
-        height: 100vh;
-        background-color: bisque;
-    }
+.map-container {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+}
 
-
-    .loading-map{
-        color: white;
-        position: fixed;
-        top: 0px;
-        left: 0px;
-        width: 100vh;
-        height: 100vh;
-        z-index: 9999;
-        background-color: rgba(0, 0, 0, 0.8);
-    }
+.loading-map {
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    height: 100vh;
+    left: 0;
+    position: fixed;
+    top: 0px;
+    width: 100vw;
+    z-index: 9999;
+}
 </style>
